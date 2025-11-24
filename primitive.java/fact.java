bd43d548c878b7dd26e5fc396ce4fb42f8c86979
Rajeev Singh <rajeevsingh19135@gmail.com>
2025-11-24 16:06:46 +0530

    fact

---
+++ primitive.java/fact.java
@@ -0,0 +1,9 @@
+public class fact {
+    public static void main(String[] args) {
+       int fact=1;
+       for(int i=0;i<=5;i++){
+        fact=fact*i;
+        System.out.println(fact);
+       } 
+    }
+}
